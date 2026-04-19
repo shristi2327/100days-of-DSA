ef85eb38e2993f311573fab46c6b6a80818a6715
shristi2327 <shristij2327@gmail.com>
2026-04-19 15:36:40 +0530

    Add files via upload

---
+++ Day_31.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+// Push operation
+void push(int value) {
+    if (top == MAX - 1) {
+        printf("Stack Overflow\n");
+        return;
+    }
+    stack[++top] = value;
+}
+
+// Pop operation
+void pop() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+        return;
+    }
+    printf("%d\n", stack[top--]);
+}
+
+// Display operation (top to bottom)
+void display() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+        return;
+    }
+
+    for (int i = top; i >= 0; i--) {
+        printf("%d", stack[i]);
+        if (i != 0) printf(" ");
+    }
+    printf("\n");
+}
+
+// Main function
+int main() {
+    int n, op, value;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &op);
+
+        if (op == 1) {
+            scanf("%d", &value);
+            push(value);
+        } 
+        else if (op == 2) {
+            pop();
+        } 
+        else if (op == 3) {
+            display();
+        }
+    }
+
+    return 0;
+}
